ce6fc225771e56f5315fa9b09fafd83aa88c4ba3
radegast0 <fatih.yonucuoglu@gmail.com>
2024-06-23 22:33:58 +0300

    vectors calculation

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,9 +4,12 @@ import { useEffect, useRef } from 'react';
 import { Vector3 } from 'three';
 import useKeyboard from '../hooks/useKeyboard';
 
+const JUMP_VELOCITY = 4;
+const SPEED = 4;
+
 const Player = () => {
-	const actions = useKeyboard();
-	
+	const { backward, forward, right, left, jump } = useKeyboard();
+
 	const { camera } = useThree();
 	const [ref, move] = useSphere(() => ({
 		mass: 1,
@@ -23,12 +26,39 @@ const Player = () => {
 	useEffect(() => {
 		move.position.subscribe((p) => (pos.current = p));
 	}, [move.position]);
+
+	// fix bug > at the top of a jump, velocity is 0 so double jump is possible
 	useFrame(() => {
 		camera.position.copy(
 			new Vector3(pos.current[0], pos.current[1], pos.current[2])
 		);
-		// move.velocity.set(0, 1, 0);
+
+		const directions = new Vector3();
+		const frontVector = new Vector3(
+			0,
+			0,
+			(backward ? 1 : 0) - (forward ? 1 : 0)
+		);
+
+		const sideVector = new Vector3(
+			(left ? 1 : 0) - (right ? 1 : 0),
+			0,
+			0
+		);
+
+		directions
+			.subVectors(frontVector, sideVector)
+			.normalize()
+			.multiplyScalar(SPEED)
+			.applyEuler(camera.rotation);
+
+		move.velocity.set(directions.x, vel.current[1], directions.z);
+
+		if (jump && Math.abs(vel.current[1].toFixed(2)) < 0.05) {
+			move.velocity.set(vel.current[0], JUMP_VELOCITY, vel.current[2]);
+		}
 	});
+
 
 	return <mesh ref={ref} />;
 };
